b1cc34f905e95cdb52109284265e5187a027975b
dylanakiralee <dylanakiralee@gmail.com>
2023-06-07 00:47:16 -0700

    Remove files

--- Proj1/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "/home/ise/CSM152A/Proj1/FPCVT_TB_isim_beh.exe" -prj "/home/ise/CSM152A/Proj1/FPCVT_TB_beh.prj" "work.FPCVT_TB" "work.glbl" 
